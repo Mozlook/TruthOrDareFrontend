2acce9c2ce08e67a78ce8467ee42bf723bd80727
Mikołaj <mikolaj.moozoluk@gmail.com>
2025-12-15 00:54:55 +0100

    Next question button

--- src/components/DisplayQuestion/QuestionCard.tsx
+++ src/components/DisplayQuestion/QuestionCard.tsx
@@ -5,7 +5,19 @@ type Props = {
   question: Question;
   DrawQuestion: () => void;
 };
-const QuestionCard: React.FC<Props> = ({ party, gameMode, question }) => {
-  return <div>{question.text}</div>;
+const QuestionCard: React.FC<Props> = ({
+  party,
+  gameMode,
+  question,
+  DrawQuestion,
+}) => {
+  return (
+    <div>
+      {question.text}
+      <button onClick={DrawQuestion} className="border-1">
+        Next
+      </button>
+    </div>
+  );
 };
 export default QuestionCard;
